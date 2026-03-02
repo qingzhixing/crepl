910638ddee4221d85b6910d3f4736cd51aae2e12
qingzhixing <qzsqqmail@qq.com>
2026-03-02 22:41:30 +0800

    📦 NEW: 准备解析输入的表达式和函数了，先写测试确保解析过程没问题

--- include/input_handler.hpp
+++ include/input_handler.hpp
@@ -17,8 +17,8 @@ public:
 		while (continue_flag)
 		{
 			std::cout << std::format("\n✨{}> ", ProjectInfo::project_name);
-			std::string input;
-			std::getline(std::cin, input);
+			std::string input_line;
+			std::getline(std::cin, input_line);
 
 			// 优先判断输入结束
 			if (std::cin.fail() || std::cin.eof())
@@ -28,13 +28,29 @@ public:
 				continue;
 			}
 
-			input = Trim(input);
-			if (input.empty())
+			input_line = Trim(input_line);
+			if (input_line.empty())
 			{
 				continue;
 			}
-			std::cout << std::format("read: {}\n", input);
 		}
+	}
+
+private:
+	void HandleInputLine(const std::string &input_line)
+	{
+	}
+
+	bool IsFunctionValid(const std::string &input_line)
+	{
+	}
+
+	bool IsExpressionValid(const std::string &input_line)
+	{
+	}
+
+	void IsLineAFunction(const std::string &input_line)
+	{
 	}
 };
 #endif //! INPUT_HANDLER_HPP
