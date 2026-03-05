bcbaf34fffb3534741667cbd12d12af0a3edf3cc
qingzhixing <qzsqqmail@qq.com>
2026-03-05 13:11:51 +0800

    🔨 REFACTOR: 重构生成文件并写入的部分

--- include/runtime_registrar.hpp
+++ include/runtime_registrar.hpp
@@ -17,80 +17,39 @@ public:
 	 */
 	static void ProcessLine(const std::string &input_line)
 	{
-		if (IsLineAFunction(input_line))
+		// Generate unique temp file
+		char file_name[] = "/tmp/crepl-XXXXXX.cpp";
+		int fd = mkstemps(file_name, 4);
+		if (fd == -1)
 		{
-			auto success = RegisterFunction(input_line);
+			std::cerr << "Error creating temporary file\n";
+			return;
+		}
 
-			if (!success)
-			{
-				std::cout << "❌ Compile Error" << std::endl;
-			}
+		// Check whether it's a function or an expression
+		bool is_function = input_line.starts_with("int ");
+		std::string expression_func_name;
+
+		// write the content
+		std::string code_content;
+		if (is_function)
+		{
+			// A Function
+			code_content = input_line + "\n";
 		}
 		else
 		{
-			auto [success, result] = RegisterRunExpression(input_line);
-
-			if (!result)
-			{
-				std::cout << "❌ Compile Error" << std::endl;
-			}
-			else
-			{
-				std::cout << "✅ Result: " << result << std::endl;
-			}
+			// An Expression
+			static int expression_counter = 0;
+			expression_func_name = std::format("__expr_{}", expression_counter++);
+			code_content = std::format("int {}() {{ return {}; }}\n", expression_func_name, input_line);
 		}
-	}
+		write(fd, code_content.c_str(), code_content.size());
 
-	/**
-	 * @brief 注册函数定义
-	 *
-	 * @param input_line
-	 */
-	static bool RegisterFunction(const std::string &input_line)
-	{
-		auto [fd, file_name] = GenerateOpenUniqueCpp();
-		if (fd < 0)
-		{
-			return false;
-		}
-		auto success = write(fd, input_line.c_str(), sizeof(input_line.c_str()));
-	}
+		// Compile and load
+		// TODO: Uncompleted
 
-	/**
-	 * @brief 注册表达式并运行它
-	 *
-	 * @param input_line
-	 * @return std::pair<bool, int> 第一个元素表示编译运行是否成功，第二个元素表示表达式的结果（如果编译运行成功）
-	 */
-	static std::pair<bool, int> RegisterRunExpression(const std::string &input_line)
-	{
-		auto [fd, file_name] = GenerateOpenUniqueCpp();
-		if (fd < 0)
-		{
-			return {false, 0};
-		}
-	}
-
-	static bool IsLineAFunction(const std::string &input_line)
-	{
-		return input_line.starts_with("int ");
-	}
-
-	/**
-	 * @brief 生成一个唯一的Cpp文件并返回其文件描述符
-	 *
-	 * @return int 唯一Cpp文件的文件描述符
-	 */
-	static std::pair<int, std::string> GenerateOpenUniqueCpp()
-	{
-		char file_name[] = "/tmp/crepl-XXXXXX.cpp";
-		int fd = mkstemps(file_name, 4); // 4 是 ".cpp" 的长度
-		if (fd < 0)
-		{
-			perror("mkstemps");
-		}
-		std::string file_name_str = file_name;
-		return {fd, file_name_str};
+		close(fd);
 	}
 };
 
